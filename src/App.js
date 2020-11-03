115cfb791fc775d8f7a181276b734968b12cbdee
Artyom Yeghoyan <artyomyeghoyan@gmail.com>
2020-11-03 14:12:29 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import meme from './meme.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="pane">
+          <img src={meme}></img>
+          <h2>Такова реальность</h2>
+          <h3>К счастью пандемия внесла свои коррективы</h3>
+        </div>
       </header>
     </div>
   );
